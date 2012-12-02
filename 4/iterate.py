949a47dc9ad65ea0675226e88543ed962939c68a
Tobias Bucher <tobiasbucher5991@gmail.com>
2012-12-02 21:46:40 +0100

    [4] documented program source code

--- 4/iterate.py
+++ 4/iterate.py
@@ -13,7 +13,11 @@ import xml.sax
 import xml.sax.saxutils
 import itertools
 
+# Klasse zum Schreiben von XML-Dokumenten
 class XMLWriter(xml.sax.saxutils.XMLGenerator):
+
+	# Decorator zum Schreiben des nötigen Whitespaces, letztendlich egal,
+	# da es wirklich nur Whitespace schreibt
 	def do_indentation(delta_ind=0):
 		def decorator(func):
 			def do_indentation_impl(self, *args, **kwargs):
@@ -35,6 +39,7 @@ class XMLWriter(xml.sax.saxutils.XMLGenerator):
 		return decorator
 
 	def __init__(self, out, encoding='UTF-8'):
+		# Initialisierung der Basisklasse XMLGenerator
 		super().__init__(out, encoding)
 		self.out = out
 		self.ind = 0 # indentation count
@@ -63,17 +68,29 @@ class SierpinskiIterator(xml.sax.ContentHandler):
 		self.writer = XMLWriter(out_file)
 		self.writer.startDocument()
 
+	# Wird aufgerufen, wenn ein XML-Element beginnt
 	def startElement(self, name, attributes):
 		if name == "rect":
+			# Falls das Quadrat weiß ist, so wird es durch acht
+			# weiße und ein schwarzes in der Mitte ersetzt
 			if attributes["fill"] == "white":
+				# Die neue Breite und Höhe muss ein Drittel
+				# der alten sein, damit 3x3 Quadrate in das
+				# alte Quadrat hineinpassen
 				width = float(attributes["width"]) / 3
 				height = float(attributes["height"]) / 3
 				x0 = float(attributes["x"])
 				y0 = float(attributes["y"])
-				for x, y in itertools.product(range(0, 2+1), range(0, 2+1)):
+
+				# Für x,y in jeglicher Kombination von (0,1,2)
+				for x, y in itertools.product(range(0, 2+1),
+						range(0, 2+1)):
 					fill = "white"
+					# Falls es das mittlere Quadrat ist,
+					# schwarz füllen
 					if x == y == 1:
 						fill = "black"
+					# Element schreiben
 					self.writer.startElement("rect", {
 						"x": str(x0 + x * width),
 						"y": str(y0 + y * height),
@@ -90,19 +107,26 @@ class SierpinskiIterator(xml.sax.ContentHandler):
 		self.writer.startElement(name, attributes)
 
 	def endElement(self, name):
-		if name != "rect": # rects are already ended in 'startElement'
+		if name != "rect":
+			# Da Rechtecke schon in der Funktion startElement
+			# (s. o.) beendet werden, müssen hier nur alle anderen
+			# beendet werden
 			self.writer.endElement(name)
 
 	def endDocument(self):
-#		print("end_document")
 		self.writer.endDocument()
 
 def iterate(in_file, out_file):
+	# Parsen der Datei mithilfe des Event-basierten Parser, der oben
+	# definiert wurde
 	xml.sax.parse(in_file, SierpinskiIterator(out_file))
 
 if __name__ == '__main__':
+	# Ausgeführt, wenn das Programm als Skript gestartet wurde
 	import argparse
 	import sys
+
+	# Kommandozeilenschnittstelle
 	p = argparse.ArgumentParser()
 	p.add_argument('input', nargs='?', default='-',
 		help="file to use as input (default: stdin)")
